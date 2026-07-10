96b4deb41f41a8f2942a03b9814d4a73cec95bb8
maulananabil526-png <maulananabil526@gmail.com>
2026-07-10 18:40:14 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+
+BOT_TOKEN = "8128997397:AAHJUUyO3aQaZnPR8UD207fZSWrb19TW8_Q"
+BRIDGE_FILE = "../shared/bridge.json"
+OWNER_ID = 5705956133
+OWNER_USERNAME = "Atryko"  # tanpa @
+
+# ===============================
+# TELETHON USERBOT
+# ===============================
+API_ID = 32962204
+API_HASH = "789de2014f598e72f1954654d6b0e7ed"
+# ===============================
+# BOT SETTINGS (OPTIONAL)
+# ===============================
+
+# Mode debug (print log tambahan)
+DEBUG = True
+
+# Cooldown default (detik)
+DEFAULT_COOLDOWN = 5
+
+# Maksimal generate default
+MAX_GENERATE = 2000
+
+# Folder session (Telegram userbot & WA kalau ada)
+SESSION_DIR = "sessions"
